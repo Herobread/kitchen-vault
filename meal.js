4c204081bf791ea671c95d4680c952c485fb6771
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-08-29 17:57:57 +0100

    add meals

--- meal.js
+++ meal.js
@@ -1,7 +1,97 @@
+// order like this:
+// 1 - difficulty
+// 2 - type of meal
+// 3 - speed of making
+// 4+ any extra tags
+// keep consistent tags for better usabilty
+
+/*
+  {
+    name: "",
+    tags: ["difficulty", "type", "speed", "extraTags"],
+    description: ""
+  },
+*/
+
 export const meals = [
   {
-    name: "Cheeeeeese",
-    tags: ["showstopper", "cheesy"],
-    description: "sadsadsadsad"
+    name: "Wrap",
+    tags: ["easy", "main", "fast", "healthy", "empty-the-fridge"],
+    description: "Tortilla + anything that is in the fridge."
+  },
+  {
+    name: "Chocolate chip cookies",
+    tags: ["easy", "dessert", "slow", "snack", "showstopper"],
+    description: "Indulgent chocolate chip cookies."
+  },
+  {
+    name: "Tiramisu Semifredo",
+    tags: ["medium", "dessert", "very-slow", "showstopper"],
+    description: "Extra rich and creamy dessert."
+  },
+  {
+    name: "Frirata",
+    tags: ["easy", "main", "empty-the-fridge", "fast", "breakfast"],
+    description: "Omelette + anything in fridge. Good for any amount of people."
+  },
+  {
+    name: "German pancake",
+    tags: ["easy", "main", "medium", "breakfast", "pancake", "showstopper"],
+    description: "Big puffy pancake with any delicious toppings you want"
+  },
+  {
+    name: "French omelette",
+    tags: ["insane", "main", "fast", "breakfast"],
+    description: "3 eggs rolled up into delicious custardy roll."
+  },
+  {
+    name: "Pasta",
+    tags: ["easy", "main", "fast", "slow", "showstopper"],
+    description: "Mince + sauce + pasta. Yum."
+  },
+  {
+    name: "Burger",
+    tags: ["easy", "main", "fast", "snack"],
+    description: "Meaaaaaaat!"
+  },
+  {
+    name: "Fries",
+    tags: ["easy", "side", "fast", "snack", "frozen"],
+    description: "Tesco fries cooked from frozen and ready in 20 min."
+  },
+  {
+    name: "Tomato mozarella salad",
+    tags: ["easy", "starter", "slow", "showstopper"],
+    description: "Beautify cut tomato slices and basel with mozarella or burrata centerpiece."
+  },
+  {
+    name: "Youghurt + oats + fruits",
+    tags: ["easy", "main", "super-fast", "breakfast", "healthy"],
+    description: "Ver fast and healthy start of a day."
+  },
+  {
+    name: "Tomato salad",
+    tags: ["easy", "salad", "fast", "healthy"],
+    description: "All the tomatoe types, red onion and herbs mixed."
+  },
+  {
+    name: "Beigin duck",
+    tags: ["medium", "main", "slow", "healthy", "frozen"],
+    description: "Mini wraps with delicious sauce and cucumbers and spring onion strips."
+  },
+  {
+    name: "Ceaser salad",
+    tags: ["easy", "salad", "fast"],
+    description: "Same as burger but different textures."
+  },
+  {
+    name: "Roast chicken spatchcock",
+    tags: ["medium", "main", "very-slow", "showstopper", "healthy"],
+    description: "Classic roast chicken"
+  },
+  {
+    name: "Rice",
+    tags: ["easy", "side", "fast", "healthy"],
+    description: "Steamed rice, don't forget the right proportions."
   },
 ];
