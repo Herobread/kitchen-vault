80dc44cb0e5223ed378225772e1a2e82cca2dd62
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-08-29 18:13:20 +0100

    Add vegetarian and vegan tags to existing meals and introduce new meal entries

--- meal.js
+++ meal.js
@@ -21,27 +21,27 @@ export const meals = [
   },
   {
     name: "Chocolate chip cookies",
-    tags: ["easy", "dessert", "slow", "snack", "showstopper"],
+    tags: ["easy", "dessert", "slow", "snack", "showstopper", "vegeterian"],
     description: "Indulgent chocolate chip cookies."
   },
   {
     name: "Tiramisu Semifredo",
-    tags: ["medium", "dessert", "very-slow", "showstopper"],
+    tags: ["medium", "dessert", "very-slow", "showstopper", "vegeterian"],
     description: "Extra rich and creamy dessert."
   },
   {
     name: "Frirata",
-    tags: ["easy", "main", "empty-the-fridge", "fast", "breakfast"],
+    tags: ["easy", "main", "empty-the-fridge", "fast", "breakfast", "vegeterian"],
     description: "Omelette + anything in fridge. Good for any amount of people."
   },
   {
     name: "German pancake",
-    tags: ["easy", "main", "medium", "breakfast", "pancake", "showstopper"],
+    tags: ["easy", "main", "medium", "breakfast", "pancake", "showstopper", "vegeterian"],
     description: "Big puffy pancake with any delicious toppings you want"
   },
   {
     name: "French omelette",
-    tags: ["insane", "main", "fast", "breakfast"],
+    tags: ["insane", "main", "fast", "breakfast", "vegeterian"],
     description: "3 eggs rolled up into delicious custardy roll."
   },
   {
@@ -56,22 +56,22 @@ export const meals = [
   },
   {
     name: "Fries",
-    tags: ["easy", "side", "fast", "snack", "frozen"],
+    tags: ["easy", "side", "fast", "snack", "frozen", "vegan"],
     description: "Tesco fries cooked from frozen and ready in 20 min."
   },
   {
     name: "Tomato mozarella salad",
-    tags: ["easy", "starter", "slow", "showstopper"],
+    tags: ["easy", "starter", "slow", "showstopper", "vegeterian"],
     description: "Beautify cut tomato slices and basel with mozarella or burrata centerpiece."
   },
   {
     name: "Youghurt + oats + fruits",
-    tags: ["easy", "main", "super-fast", "breakfast", "healthy"],
+    tags: ["easy", "main", "super-fast", "breakfast", "healthy", "vegeterian"],
     description: "Ver fast and healthy start of a day."
   },
   {
     name: "Tomato salad",
-    tags: ["easy", "salad", "fast", "healthy"],
+    tags: ["easy", "salad", "fast", "healthy", "vegan"],
     description: "All the tomatoe types, red onion and herbs mixed."
   },
   {
@@ -91,17 +91,62 @@ export const meals = [
   },
   {
     name: "Rice",
-    tags: ["easy", "side", "fast", "healthy"],
+    tags: ["easy", "side", "fast", "healthy", "vegan"],
     description: "Steamed rice, don't forget the right proportions."
   },
   {
+    name: "Potato mash",
+    tags: ["easy", "side", "fast", "healthy", "vegan"],
+    description: "Simple potato"
+  },
+  {
+    name: "Sweet potato mash",
+    tags: ["easy", "side", "fast", "healthy", "vegan"],
+    description: "Better potato"
+  },
+  {
     name: "Egg noodle",
-    tags: ["easy", "side", "super-fast", "healthy"],
+    tags: ["easy", "side", "super-fast", "healthy", "vegeterian"],
     description: "Better version of instant noodle"
   },
   {
     name: "Kale crips",
-    tags: ["easy", "snack", "fast", "healthy"],
+    tags: ["easy", "snack", "fast", "healthy", "vegan", "vegeterian"],
     description: "Delicious helthy crisps"
+  },
+  {
+    name: "Knafeh",
+    tags: ["medium", "dessert", "slow", "vegeterian", "showstopper"],
+    description: "Delicious cheesy dessert with honey"
+  },
+  {
+    name: "Ratatouille",
+    tags: ["easy", "main", "slow", "vegan", "vegeterian"],
+    description: "Baked bunch of veggies"
+  },
+  {
+    name: "Pancake",
+    tags: ["easy", "main", "slow", "vegeterian"],
+    description: "A brown thin, round cake"
+  },
+  {
+    name: "Dauphinoise potatoes",
+    tags: ["medium", "main", "side", "slow", "vegeterian"],
+    description: "Very cheesy potatoes."
+  },
+  {
+    name: "Grilled Halloumi cheese",
+    tags: ["easy", "side", "fast", "vegeterian"],
+    description: "Mashallah"
+  },
+  {
+    name: "Cheese platter",
+    tags: ["easy", "side", "fast", "vegeterian"],
+    description: "Create a tasting session of different cheeses from hard to soft ones."
+  },
+  {
+    name: "Chocolate cake",
+    tags: ["???", "dessert", "very-slow"],
+    description: "Mashallah"
   },
 ];
